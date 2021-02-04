a5ece8877a1bb2c8292fc7d2dc515cfe1b03e3c7
JhanReyes <jhan.reyes@mail.escuelaing.edu.co>
2021-02-04 15:56:24 +0000

    primer aporte desde local.

---
+++ ex0/src/funciones.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+/*Este progrma es el primero para practicar
+ *
+ *un archivo ejecutable a partir de codigo fuente de C/C++
+ */
+
+
+int main(int argc, char** argv){
+	int a=0;
+	int b;
+	float c=0.;
+	float d;
+ 	std::cout << "Ingrese dos valores enteros";
+	std::cin>>a>>b;
+	std::cout << "Hello World ALSE" << a <<" "<< b <<" "<< c<< " " << d <<std::endl;
+	cout << "cantidad de parametros" << argc <<andl;
+
+
+return 0;
+}
---
+++ ex0/src/world.c
@@ -0,0 +1,18 @@
+#include <iostream>
+/*Este progrma es el primero para practicar
+ *
+ *un archivo ejecutable a partir de codigo fuente de C/C++
+ */
+
+
+int main(){
+	int a=0;
+	int b;
+	float c=0.;
+	float d;
+ 	std::cout << "Ingrese dos valores enteros";
+	std::cin>>a>>b;
+	std::cout << "Hello Worls ALSE" << a <<" "<< b <<" "<< c<< " " << d <<std::endl;
+
+return 0;
+}
